48e2ff4a565d7f4324f1180e6af1213c385e7b09
Austin Kwon <austinkwon@Austins-MacBook-Pro.local>
2020-11-04 22:46:52 -0800

    Project: added displayMeters() function

--- project/backend/projectapp.js
+++ project/backend/projectapp.js
@@ -8,12 +8,13 @@ const calculateTrend = require(__dirname + "/calculateTrend.js");
 const app= express();
 const math = require('mathjs');
 
-
+/*
 var fromInput="";
 var toInput="";
 app.set('view engine', 'ejs');//tell our app to use ejs as its view engine
 //to get date from browser: filter by dates:use bodyparser
 app.use(bodyParser.urlencoded({extended:true}));
+*/
 
 // open the database
 let db = new sqlite3.Database('./data/meterDB.sqlite',sqlite3.OPEN_READWRITE,(err) => {
@@ -23,15 +24,12 @@ let db = new sqlite3.Database('./data/meterDB.sqlite',sqlite3.OPEN_READWRITE,(er
     console.log('Connected to the Meter database.');
   });
 
-//test aproximated hrs
-//let aprox = calculateTrend.calculateAproxHours("SWITCH MACH","PM","TC");
-//console.log(aprox);
-
 calc_OverallCompletionRate();
+displayMeters();
 displayMeterInfo();
+displayActivity();
 
 app.get("/", (req, res) => {
-  if(fromInput == "" && toInput == ""){
     const sql = "SELECT WO_Num, Department, Goal_Group, Goal, Units, m_Meter_Name, m_Meter_Reading, m_Reading_Date, Completion FROM MeterWO, Meters, Project WHERE m_Goal_Group = Goal_Group AND PS_Project = p_PS_Project ORDER BY m_Reading_Date"
     db.all(sql, [], (err, rows) => {
       if (err) {
@@ -39,17 +37,6 @@ app.get("/", (req, res) => {
       }
       res.render("list", {model: rows});//list under views and pass mode=variable
     });
-  } 
-
-  else {
-    const sql = "SELECT WO_Num, Department, Goal_Group, Goal, Units, m_Meter_Name, m_Meter_Reading, m_Reading_Date, Completion FROM MeterWO, Meters, Project WHERE m_Goal_Group = Goal_Group AND PS_Project = p_PS_Project ORDER BY m_Reading_Date"
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.render("list", {model: rows});//list under views and pass mode=variable
-    });
-  }
 });
 
 function calc_OverallCompletionRate() {
@@ -59,10 +46,7 @@ function calc_OverallCompletionRate() {
 
   // Select meter readings from the same goal group
   const sql = "SELECT m_Meter_Reading, Goal FROM Meters WHERE m_Goal_Group = 'A1 DRAIN,A1 DRAIN 2' AND m_Completion IS NULL"
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      return console.error(err.message);
-    }
+  db.all(sql, [], rows => {
 
     for (const item of rows) {
       (item.Completion).push(math.round((item.m_Meter_Reading / item.Goal) * 100));
@@ -70,7 +54,7 @@ function calc_OverallCompletionRate() {
       totalProgress += item.m_Meter_Reading;
       totalGoal += item.Goal;
 
-      // console.log(item.Completion)
+      // console.log(item.Completion) // DEBUG: view completion rate
     }
 
     completionRate = math.round((totalProgress/totalgoal) * 100); // Round completion by percentage
@@ -81,13 +65,19 @@ function calc_OverallCompletionRate() {
   });
 }
 
+function displayMeters() {
+  const sql = "SELECT p_PS_Project, p_Description, Status FROM Projects, MeterWO WHERE p_PS_Project = PS_Project"
+  db.get(sql, [], row => {
+    console.log(row.p_PS_Project);
+    console.log(row.p_Description);
+    console.log(row.Status);
+  });
+}
+
+// The specific info must be displayed for the meter that is clicked on
 function displayMeterInfo() {
   const sql = "SELECT WO_Num, Department, Goal_Group, Goal, Units, m_Meter_Name, m_Meter_Reading, m_Reading_Date, Completion FROM MeterWO, Meters, Project WHERE m_Goal_Group = Goal_Group AND PS_Project = p_PS_Project"
-  db.get(sql, [], (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-
+  db.get(sql, [], row => {
     console.log(row.WO_Num);
     console.log(row.Department);
     console.log(row.Goal_Group);

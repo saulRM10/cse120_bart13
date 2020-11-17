a7fe65a587e7f9c97c4ea5e4cd71e966c39ed446
saulRM10 <saulreyes42315@gmail.com>
2020-11-17 11:47:09 -0800

    updated the dropdown feature also added the new side menu feature

--- public/css/main.js
+++ public/css/main.js
@@ -1,15 +1,26 @@
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-const optionsList = document.querySelectorAll(".option");
-
-selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-});
-
-optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
   });
-});
+}
+
+// side navigation 
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = " 250px";
+
+}
+
+// close side navigation 
+function closeNav(){
+  document.getElementById("mySidenav").style.width="0";
+}
